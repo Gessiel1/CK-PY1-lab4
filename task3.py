95471fe45579765f71ed9dc7f75c9f6a1cc5bb3d
Gessiel1 <115106908+Gessiel1@users.noreply.github.com>
2022-11-02 18:49:28 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,6 +1,6 @@
 def delete(list_, index=None):
-    return[list_[:-1]]
+    return list_[:-1]
 
-print(delete([0,1,2,2], index=0))  # [0, 1]
-print(delete([0, 1, 2, 3,3], index=1))  # [0, 1, 2]
-print(delete([0, 1, 2, 3, 4, 4], index=1))  # [0, 1, 2, 3]
+print(delete([0,1,2,2], index=0)) 
+print(delete([0, 1, 2, 3,3], index=1))  
+print(delete([0, 1, 2, 3, 4, 4], index=1))  
